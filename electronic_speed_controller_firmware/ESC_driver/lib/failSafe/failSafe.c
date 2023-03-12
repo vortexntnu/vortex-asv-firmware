c815febf85a7bde40585782a8af90987d58a9aa8
Theo Martin <theoam@stud.ntnu.no>
2023-03-12 16:45:45 +0100

    Created failsafe and operating mode functions

---
+++ electronic_speed_controller_firmware/ESC_driver/lib/failSafe/failSafe.c
@@ -0,0 +1,5 @@
+#include <Arduino.h>
+
+void failSafeStatus(int failSafeIn,int failSafeOut, int softKillIn, int softKillOut);
+
+void operatingModeStatus(int hardOpModeIn, int hardOpModeOut, int softOpModeIn, int softOpModeOut);
