439e51c13d5e6675ce3d747e3549614de646bbb2
Martynas Smilingis <42731091+PizzaAllTheWay@users.noreply.github.com>
2023-03-30 19:50:18 +0200

    Update main.cpp

--- electronic_speed_controller_firmware/ESC_driver/src/main.cpp
+++ electronic_speed_controller_firmware/ESC_driver/src/main.cpp
@@ -4,10 +4,10 @@
 #include "failSafe.h"
 
 //Refer to the last 8 rows of the table https://vortex.a2hosted.com/index.php/ASV_Fail_Safe#Teensy_connections_(SW)
-#define HARDWAREKILLSWITCHTRIG 2
-#define SOFTWAREKILLSWITCHTRIG 3
-#define SOFTWAREOPERATINGMODE 4
-#define HARDWAREOPERATINGMODE 5
+#define HARDWAREKILLSWITCHTRIG 23
+#define SOFTWAREKILLSWITCHTRIG 22
+#define SOFTWAREOPERATINGMODE 21
+#define HARDWAREOPERATINGMODE 20
 
 #define ESC_PIN1 28
 #define ESC_PIN2 29
