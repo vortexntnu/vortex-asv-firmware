84d577cda8aee54e8b42fc057d161057f53138ab
emilssp <espasov991@gmail.com>
2023-09-20 12:35:22 +0200

    Added buzzer switch

--- elhouse-cover-sensor-and-alarm/src/main.cpp
+++ elhouse-cover-sensor-and-alarm/src/main.cpp
@@ -7,7 +7,8 @@ const int echoPin1 = 5;
 const int trigPin2 = 7;
 const int echoPin2 = 8;
 
-const int buzzer = 6;
+const int switchPin = 3;
+const int buzzerPin = 6;
 
 //Distance when cover is on 
 const unsigned int distanceClosed = 130;
@@ -16,27 +17,16 @@ const unsigned int distanceClosed = 130;
 long duration;
 unsigned int distance;
 
-bool isLidClosed();
-
 void setup() {
   pinMode(trigPin1, OUTPUT); // Sets the trigPin1 as an Output
   pinMode(echoPin1, INPUT); // Sets the echoPin1 as an Input
   pinMode(trigPin2, OUTPUT); // Sets the trigPin2 as an Output
   pinMode(echoPin2, INPUT); // Sets the echoPin2 as an Input
-  pinMode(buzzer, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(switchPin, INPUT);
+
   Serial.begin(9600); // Starts the serial communication
   Serial.println("Starting process");
-}
-
-void BuzzIfLidOpen() {
-  if (isLidClosed(trigPin1, echoPin1) && isLidClosed(trigPin2, echoPin2)) {
-    tone(buzzer, 1000);
-    delay(100);
-  }
-  else{
-    noTone(buzzer);
-  }
-
 }
 
 bool isLidClosed(int trigPin, int echoPin) {
@@ -52,5 +42,26 @@ bool isLidClosed(int trigPin, int echoPin) {
   // Calculating the distance
   distance = duration * 0.34 / 2;
   Serial.println(distance);
+
   return distance>distanceClosed;
 }
+
+void BuzzIfLidOpen() {
+
+  int stateOfSwitch = digitalRead(switchPin);
+
+  if (stateOfSwitch == HIGH){
+    if (isLidClosed(trigPin1, echoPin1) && isLidClosed(trigPin2, echoPin2)) {
+      tone(buzzerPin, 1000);
+      delay(100);
+    }
+    else{
+      noTone(buzzerPin);
+    }
+  }
+  else{
+    noTone(buzzerPin);
+  }
+}
+
+void loop(){}
