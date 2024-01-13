dae7962a3ec2d496d62fd06ed80296f9282280fd
IbixD97 <hemo.alsaraj@gmail.com>
2024-01-13 18:43:02 +0100

    Fixing the PlatformIO.ini file so that it works for Arduino Micro Fixed pin numbers Code is still incomplete and needs a lot of testing and work probably.

--- electronic_speed_controller_firmware/ESC_driver/src/main.cpp
+++ electronic_speed_controller_firmware/ESC_driver/src/main.cpp
@@ -3,7 +3,7 @@
 #include <Wire.h>
 #include "failSafe.h"
 
-//Refer to the last 8 rows of the table https://vortex.a2hosted.com/index.php/ASV_Fail_Safe#Teensy_connections_(SW)
+// Refer to the last 8 rows of the table https://vortex.a2hosted.com/index.php/ASV_Fail_Safe#Teensy_connections_(SW)
 #define HARDWAREKILLSWITCHTRIG 23
 #define SOFTWAREKILLSWITCHTRIG 22
 #define HARDWAREOPERATINGMODE 20
@@ -12,17 +12,16 @@
 #define SOFTWAREOPERATINGMODE 21 // Unecessary pin, Teensy doesnt need to know Software mode because it just need to know PWM signal, no matter the mode it will just go with the PWM signal
 // DELETE THIS (STOP) ==================================================
 
-#define ESC_PIN1 28
-#define ESC_PIN2 29
-#define ESC_PIN3 36
-#define ESC_PIN4 37
-#define ARMING_PWM 1500 // 1500 ms
-#define STARTUP_DELAY 15000 // 15 s
+#define ESC_PIN1 5
+#define ESC_PIN2 6
+#define ESC_PIN3 9
+#define ESC_PIN4 10
+#define ARMING_PWM 1500             // 1500 ms
+#define STARTUP_DELAY 15000         // 15 s
 #define FAILSAFE_CHECK_INTERVAL 500 // 500 ms
 #define I2C_SDA_PIN 18
 #define I2C_SCL_PIN 19
 #define I2C_ADDRESS 0x21
-
 
 void arm_thrusters();
 void disarm_thrusters();
@@ -37,7 +36,8 @@ Servo ESC4;
 unsigned long failsafeTimerLastCheck = 0;
 bool isArmed = false;
 
-void setup() {
+void setup()
+{
   // Set up debugging
   Serial.begin(9600);
   delay(5000);
@@ -61,43 +61,54 @@ void setup() {
   Serial.println("Setting up I2C communication...");
   Wire.setSDA(I2C_SDA_PIN);
   Wire.setSCL(I2C_SCL_PIN);
-  Wire.begin(I2C_ADDRESS); // Join the I2C bus as a slave
+  Wire.begin(I2C_ADDRESS);        // Join the I2C bus as a slave
   Wire.onReceive(receiveMessage); // Register the receive event handler
 }
 
-void loop() {
-  if (millis() - failsafeTimerLastCheck > FAILSAFE_CHECK_INTERVAL) {
+void loop()
+{
+  if (millis() - failsafeTimerLastCheck > FAILSAFE_CHECK_INTERVAL)
+  {
     /*
     -1: ERROR
-    
+
     0: Hardware operation mode
     1: Software operation mode
     */
     int operationMode = operatingModeStatus(HARDWAREOPERATINGMODE);
 
     bool failsafeTriggered = failSafeStatus(HARDWAREKILLSWITCHTRIG, SOFTWAREKILLSWITCHTRIG);
-    
-    if (isArmed && (failsafeTriggered || operationMode == -1 || operationMode == 0)) {
+
+    if (isArmed && (failsafeTriggered || operationMode == -1 || operationMode == 0))
+    {
       Serial.println("Disarming!!!");
       disarm_thrusters();
     }
-    else if (!isArmed && !failsafeTriggered && operationMode == 1) {
+    else if (!isArmed && !failsafeTriggered && operationMode == 1)
+    {
       Serial.println("Arming...");
       arm_thrusters();
     }
 
     failsafeTimerLastCheck = millis();
-    Serial.print("HK (23): ");Serial.print(digitalRead(HARDWAREKILLSWITCHTRIG));
-    Serial.print("  |  SK (22): ");Serial.print(digitalRead(SOFTWAREKILLSWITCHTRIG));
-    Serial.print("  |  SM (21): ");Serial.print(digitalRead(SOFTWAREOPERATINGMODE));
-    Serial.print("  |  HM (20): ");Serial.print(digitalRead(HARDWAREOPERATINGMODE));
-    Serial.print("  |  Mode: ");Serial.print(operationMode);
-    Serial.print("  |  FailsafeTRIG?: ");Serial.print(failsafeTriggered);
+    Serial.print("HK (23): ");
+    Serial.print(digitalRead(HARDWAREKILLSWITCHTRIG));
+    Serial.print("  |  SK (22): ");
+    Serial.print(digitalRead(SOFTWAREKILLSWITCHTRIG));
+    Serial.print("  |  SM (21): ");
+    Serial.print(digitalRead(SOFTWAREOPERATINGMODE));
+    Serial.print("  |  HM (20): ");
+    Serial.print(digitalRead(HARDWAREOPERATINGMODE));
+    Serial.print("  |  Mode: ");
+    Serial.print(operationMode);
+    Serial.print("  |  FailsafeTRIG?: ");
+    Serial.print(failsafeTriggered);
     Serial.println();
   }
 }
 
-void arm_thrusters() {
+void arm_thrusters()
+{
   isArmed = true;
   ESC1.writeMicroseconds(ARMING_PWM);
   ESC2.writeMicroseconds(ARMING_PWM);
@@ -105,45 +116,55 @@ void arm_thrusters() {
   ESC4.writeMicroseconds(ARMING_PWM);
 }
 
-void disarm_thrusters() {
+void disarm_thrusters()
+{
   isArmed = false;
   drive_thrusters(0, 0, 0, 0);
 }
 
-void drive_thrusters(int PWM_ESC1, int PWM_ESC2, int PWM_ESC3, int PWM_ESC4) {
+void drive_thrusters(int PWM_ESC1, int PWM_ESC2, int PWM_ESC3, int PWM_ESC4)
+{
   ESC1.writeMicroseconds(PWM_ESC1);
   ESC2.writeMicroseconds(PWM_ESC2);
   ESC3.writeMicroseconds(PWM_ESC3);
   ESC4.writeMicroseconds(PWM_ESC4);
 }
 
-void receiveMessage(int bytes) {
-  if (bytes == 8) { // Check if 8 bytes (4 x 16-bit integers) are received
+void receiveMessage(int bytes)
+{
+  if (bytes == 8)
+  { // Check if 8 bytes (4 x 16-bit integers) are received
     int PWM_values[4];
 
     // Read the I2C data and convert bytes to 16-bit integers
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; i++)
+    {
       int msb = Wire.read();
       int lsb = Wire.read();
       PWM_values[i] = (msb << 8) | lsb;
     }
 
     // Drive thrusters
-    if (isArmed) {
+    if (isArmed)
+    {
       drive_thrusters(PWM_values[0], PWM_values[1], PWM_values[2], PWM_values[3]);
 
       // Debugging
       Serial.print("PWM values: ");
-      for (int i = 0; i < 4; i++) {
+      for (int i = 0; i < 4; i++)
+      {
         Serial.print(PWM_values[i]);
         Serial.print(" ");
       }
       Serial.println();
     }
-    else {
+    else
+    {
       Serial.println("Thrusters not armed!");
-    }  
-  } else {
+    }
+  }
+  else
+  {
     disarm_thrusters();
     Serial.println("Invalid data length received!");
   }
