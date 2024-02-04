f5fe34a6dfa7faf8e56bcbbd62822441cf4ffdca
IbixD97 <78154788+IbixD97@users.noreply.github.com>
2024-02-04 11:05:36 +0100

    Update i2c.h

--- fs-firmware-v4-c/lib/i2c/i2c.h
+++ fs-firmware-v4-c/lib/i2c/i2c.h
@@ -1,5 +1,12 @@
-#ifndef I2C_H
-#define I2C_H
+#ifndef FS_Status_Light_H
+#define FS_Status_Light_H
 
 #include "gpio.h"
-#include <Wire.h>
+
+void set_rgby(char colour[1], int state);
+void test_24v_status();
+void test_leds();
+void run();
+void status_lights();
+
+#endif
