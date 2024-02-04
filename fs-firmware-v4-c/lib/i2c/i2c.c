7ef7996f858b0d93605de79a3222ecb03c0c0de6
IbixD97 <78154788+IbixD97@users.noreply.github.com>
2024-02-04 10:59:03 +0100

    Update i2c.c

--- fs-firmware-v4-c/lib/i2c/i2c.c
+++ fs-firmware-v4-c/lib/i2c/i2c.c
@@ -1,1 +1,15 @@
-#include "i2c.h"
+
+#include <util/twi.h>
+#include <avr/interrupt.h>
+
+
+#ifndef I2C_H_
+#define I2C_H_
+
+
+
+void i2c_init();
+void i2c_receive();
+void i2c_transmit();
+
+#endif /* I2C_H_ */ 
