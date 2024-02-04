d69d8a4011dd5ab5e9ba5e27113f17f3bea7fcb2
IbixD97 <78154788+IbixD97@users.noreply.github.com>
2024-02-04 10:59:28 +0100

    Update i2c.c

--- fs-firmware-v4-c/lib/i2c/i2c.c
+++ fs-firmware-v4-c/lib/i2c/i2c.c
@@ -1,15 +1,100 @@
 
-#include <util/twi.h>
-#include <avr/interrupt.h>
+#include "i2c/i2c.h"
 
+volatile uint8_t status = 0;
 
-#ifndef I2C_H_
-#define I2C_H_
+ISR(TWI_vect)
+{
 
+    if ((TWSR & TW_NO_INFO) == TW_SR_SLA_ACK || (TWSR & TW_NO_INFO) == TW_SR_ARB_LOST_SLA_ACK)
+    {
+        TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN) | (1 << TWIE);
+    }
+    else if ((TWSR & TW_NO_INFO) == TW_SR_DATA_ACK || (TWSR & TW_NO_INFO) == TW_SR_DATA_NACK)
+    {
+        status = TWDR;
 
+        TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN) | (1 << TWIE);
+    }
+    else
+    {
+        TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWIE);
+    }
+}
 
-void i2c_init();
-void i2c_receive();
-void i2c_transmit();
+void twi_init(uint8_t sub_address)
+{
+    // sub for submissi... I mean subscriber...
+    TWAR = sub_address << 1;
+    TWCR = (1 << TWEN) | (1 << TWIE) | (1 << TWEA);
+    sei();
+}
 
-#endif /* I2C_H_ */ 
+int8_t i2c_listen()
+{
+    while (1)
+    {
+        uint8_t status;
+        while (!(TWCR & (1 << TWINT)))
+            ;
+        status = TWSR & 0xF8;
+        if (status == 0x60 || status == 0x68)
+        {
+            return 0;
+        }
+        if (status == 0xA8 || status == 0xB0)
+        {
+            return 1;
+        }
+        if (status == 0x70 || status == 0x78)
+        {
+            return 2;
+        }
+        else
+        {
+            continue;
+        }
+    }
+}
+
+int8_t i2c_transmit(char data)
+{
+    uint8_t status;
+    TWDR = data;
+    TWCR = (1 << TWEN) | (1 << TWINT) | (1 << TWEA);
+
+    while (!(TWCR & (1 << TWINT)))
+        ;
+    status = TWSR & 0xF8;
+    if (status == 0xA0)
+    {
+        TWCR |= (1 << TWINT);
+        return -1;
+    }
+}
+
+void twi_match_read_slave(void)
+{
+    while ((TWSR & TW_NO_INFO) != TW_SR_SLA_ACK)
+    {
+        TWCR = (1 << TWEA) | (1 << TWEM) | (1 << TWINT);
+        while (!(TWCR & (1 << TWINT)))
+            ;
+    }
+}
+
+void twi_read(void)
+{
+    TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN);
+    while (!(TWCR & (1 << TWINT)))
+        ;
+    while ((TWSR & TW_NO_INFO) != TW_SR_DATA_ACK)
+        ;
+    recv_data = TWDR;
+    PORTB = recv_data;
+}
+
+void twi_match_wrtie_slave(void)
+{
+    while ((TWSR & TW_NO_INFO) !=)
+}
